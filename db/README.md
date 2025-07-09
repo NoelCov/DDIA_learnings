fb4166349704c28440f5a2e307ced67f3d70da4b
Noel Codes <no3lcodes@gmail.com>
2025-07-08 20:00:46 -0500

    Made a few changes for new lines in print statements. Modified a bit of logic around the creation of segment files, added a function to delete a key which needs to be updated.

--- db/README.md
+++ db/README.md
@@ -1,1 +1,10 @@
-# Hi
+# LSM-Tree attempt xD
+
+The third chapter of DDIA was my favorite, I have bene using databases since the start of my career but I feel like I never even attempted to understand how databases store, retrieve, update the data we give it. This chapter showed me how amazing these systems are, and I wanted to attempt to make a LSM-Tree to make sure I learned some of the concepts this chapter introduced, such as:
+- Log-based storage
+- Usage of red-black trees to store key/value pairs in order to be able to do retrievals in O(log n)
+- Segment files (generation, compacting of them)
+
+## Running the app
+You can download the code source, and with golang use ```go run main.go``` which will open up a menu to give you options to insert data into the db, view the state of the tree, search for a key in the db.
+
--- db/main.go
+++ db/main.go
@@ -29,6 +29,8 @@ func main() {
 			db.writeKeyValue()
 		} else if selection == "3" {
 			db.getStateOfTree()
+		} else if selection == "4" {
+			db.deleteKey()
 		} else {
 			return
 		}
@@ -42,17 +44,19 @@ func (db *LogbasedDB) writeKeyValue() {
 	if err != nil {
 		log.Fatal("Error reading key input.")
 	}
+	key = strings.TrimSpace(key)
 
 	fmt.Print("Enter your value: ")
 	value, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal("Error reading value input.")
 	}
+	value = strings.TrimSpace(value)
 
 	indexTree := db.indexTree
 	indexTree.Put(key, value)
 
-	fmt.Println("Value was inserted in database.\n")
+	fmt.Println("\nValue was inserted in database.")
 
 	if indexTree.Size() == 2 {
 		db.generateSegmentFileFromTree()
@@ -67,12 +71,13 @@ func (db LogbasedDB) getKeyValue() {
 	if err != nil {
 		log.Fatal("Error reading key input.")
 	}
+	key = strings.TrimSpace(key)
 
 	keyValue, found := db.indexTree.Get(key)
 	if found {
-		fmt.Println(keyValue.(string))
+		fmt.Printf("\nValue for key '%s' is: %s\n\n", key, keyValue.(string))
 	} else {
-		fmt.Println("The key wasn't found in the tree. Looking into segment files")
+		fmt.Println("\nThe key wasn't found in the tree. Looking into segment files")
 
 		segmentFileNames := db.getSegmentFileNames()
 		// Latest file should always be checked first, then second latest, etc. This is because we want to find the latest key/value pair that exists in case it was updated.
@@ -80,6 +85,7 @@ func (db LogbasedDB) getKeyValue() {
 			segmentFile, err := os.Open("segments/" + segmentFileNames[i] + ".txt")
 			if err != nil {
 				fmt.Printf("Error opening segment file '%s': %s", segmentFileNames[i], err)
+				return
 			}
 			defer segmentFile.Close()
 
@@ -90,7 +96,6 @@ func (db LogbasedDB) getKeyValue() {
 			}
 
 			left, right := 0, len(lines)-1
-			fmt.Println(lines)
 			key = strings.TrimSpace(key)
 
 			for left <= right {
@@ -133,19 +138,34 @@ func (db *LogbasedDB) generateSegmentFileFromTree() {
 	if err != nil {
 		log.Fatal("Could not parse integer from sequence number of segment file.")
 	}
+
+	// Save segment file name in segments.manifest
+	segmentsManiestFile, err := os.OpenFile("segments.manifest", os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal("\nError opening segments file to write new segment file to.")
+	}
+	defer segmentsManiestFile.Close()
+
+	_, err = segmentsManiestFile.WriteString(db.segmentFileName + "\n")
+	if err != nil {
+		log.Fatal("\nError writing to segment manifest file: ", err)
+	}
+
 	// Update the values for the next segment file to be written.
 	sequenceNum += 1
 	db.segmentFileName = "segment-" + strconv.Itoa(sequenceNum)
-	fmt.Println("Segment file was created.\n")
+	fmt.Println("\nSegment file was created.")
+
 }
 
 func getMenuSelection() string {
-	fmt.Println("Select one of the options below:\n")
+	fmt.Println("\nSelect one of the options below:")
 
-	fmt.Println("1 - Search for key in database.")
+	fmt.Println("\n1 - Search for key in database.")
 	fmt.Println("2 - Enter key/value pair in database.")
 	fmt.Println("3 - Look at state of database.")
-	fmt.Println("4 - Quit application.")
+	fmt.Println("4 - Delete key in database.")
+	fmt.Println("5 - Quit application.")
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("\nEnter your selection: ")
@@ -171,23 +191,19 @@ func (db *LogbasedDB) init() {
 	for scanner.Scan() {
 		segments = append(segments, scanner.Text())
 	}
-	fmt.Println("Segments from segments file: ", len(segments))
 
 	if len(segments) == 0 {
-		segmentFileName := "segment-0001"
-		_, err := os.Create("segments/" + segmentFileName + ".txt")
-		if err != nil {
-			log.Fatal("Error creating segment file: ", err)
-		}
-		_, err = segmentsFile.WriteString(segmentFileName + "\n")
-		if err != nil {
-			log.Fatal("Could not write to segment file: ", err)
-		}
-		db.segmentFileName = segmentFileName
+		db.segmentFileName = "segment-1"
 	} else {
-		db.segmentFileName = segments[len(segments)-1]
+		// Start a new segment that continues from the last one
+		lastSegmentNumber, err := strconv.Atoi(strings.Split(segments[len(segments)-1], "-")[1])
+		if err != nil {
+			log.Fatal("\nCould not retrieve last segment number.")
+		}
+
+		db.segmentFileName = strconv.Itoa(lastSegmentNumber + 1)
 	}
-	fmt.Println("Database was initialized correctly.\n")
+	fmt.Println("Database was initialized correctly.")
 }
 
 func (db *LogbasedDB) getSegmentFileNames() []string {
@@ -203,4 +219,44 @@ func (db *LogbasedDB) getSegmentFileNames() []string {
 		segments = append(segments, scanner.Text())
 	}
 	return segments
+}
+
+func (db *LogbasedDB) deleteKey() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("\nEnter key to delete: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("\nError reading input: ", err)
+		return
+	}
+	key := strings.TrimSpace(input)
+
+	rbTree := db.indexTree
+	_, found := rbTree.Get(key)
+	if found {
+		rbTree.Remove(key)
+		fmt.Println("\nKey was deleted from db.")
+		return
+	} else {
+		fmt.Println("\nKey wasn't found in memtable, looking in segment files.")
+		segmentsFileNames := db.getSegmentFileNames()
+		for i := len(segmentsFileNames) - 1; i >= 0; i-- {
+			segmentFile, err := os.OpenFile("segments/"+segmentsFileNames[i]+".txt", os.O_RDWR, 0644)
+			if err != nil {
+				log.Fatal("Error opening segment file: ", err)
+			}
+			defer segmentFile.Close()
+
+			scanner := bufio.NewScanner(segmentFile)
+			for scanner.Scan() {
+				text := scanner.Text()
+				if strings.Contains(text, key) {
+					fmt.Println("Found key in segment file to delete.")
+					segmentFile.WriteString("")
+					return
+				}
+			}
+		}
+	}
+	fmt.Println("\nKey was not found in db.")
 }
